b642aa60e6cd18995b4ed5ca7cb78a9c904ee531
CanadianBaconBoi <bc.bacon.bits@gmail.com>
2022-04-15 23:57:11 -0700

    Add Import

--- src/main/java/extendedtools/common/item/PaxelItem.java
+++ src/main/java/extendedtools/common/item/PaxelItem.java
@@ -12,6 +12,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
