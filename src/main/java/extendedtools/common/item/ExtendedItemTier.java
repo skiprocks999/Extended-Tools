f1a481ddf93192297dee05975b90f43675ed5d48
skip999 <skiprocks999@gmail.com>
2023-12-24 11:38:31 -0600

    update to latest forge version

--- src/main/java/extendedtools/common/item/ExtendedItemTier.java
+++ src/main/java/extendedtools/common/item/ExtendedItemTier.java
@@ -5,61 +5,69 @@ import net.minecraft.item.ItemTier;
 import net.minecraft.item.crafting.Ingredient;
 
 public enum ExtendedItemTier implements IItemTier {
-    STEEL(2, (int) (ItemTier.IRON.getMaxUses() * 2.7), ItemTier.IRON.getEfficiency(), ItemTier.IRON.getAttackDamage() * 1.5f, 5),
-    BRONZE(2, ItemTier.IRON.getMaxUses(), ItemTier.IRON.getEfficiency(), ItemTier.IRON.getAttackDamage(), 5),
-    TIN(1, ItemTier.STONE.getMaxUses(), ItemTier.STONE.getEfficiency(), ItemTier.IRON.getAttackDamage(), 5),
-    COPPER(1, ItemTier.STONE.getMaxUses() * 2, ItemTier.STONE.getEfficiency(), ItemTier.STONE.getAttackDamage(), 5),
-    LEAD(2, ItemTier.DIAMOND.getMaxUses() * 2, ItemTier.STONE.getEfficiency(), ItemTier.IRON.getAttackDamage(), 5),
-    VANADIUM(2, ItemTier.STONE.getMaxUses(), ItemTier.IRON.getEfficiency(), ItemTier.IRON.getAttackDamage(), 5),
-    SILVER(2, (int) (ItemTier.IRON.getMaxUses() * 1.3), ItemTier.GOLD.getEfficiency(), ItemTier.GOLD.getAttackDamage(), 5),
-    TITANIUM(2, ItemTier.IRON.getMaxUses() * 4, ItemTier.IRON.getEfficiency() * 1.1f, ItemTier.IRON.getAttackDamage() * 1.6f, 5);
+	STEEL(2, (int) (ItemTier.IRON.getUses() * 2.7), ItemTier.IRON.getSpeed(), ItemTier.IRON.getAttackDamageBonus() * 1.5f, 5),
+	//
+	BRONZE(2, ItemTier.IRON.getUses(), ItemTier.IRON.getSpeed(), ItemTier.IRON.getAttackDamageBonus(), 5),
+	//
+	TIN(1, ItemTier.STONE.getUses(), ItemTier.STONE.getSpeed(), ItemTier.IRON.getAttackDamageBonus(), 5),
+	//
+	COPPER(1, ItemTier.STONE.getUses() * 2, ItemTier.STONE.getSpeed(), ItemTier.STONE.getAttackDamageBonus(), 5),
+	//
+	LEAD(2, ItemTier.DIAMOND.getUses() * 2, ItemTier.STONE.getSpeed(), ItemTier.IRON.getAttackDamageBonus(), 5),
+	//
+	VANADIUM(2, ItemTier.STONE.getUses(), ItemTier.IRON.getSpeed(), ItemTier.IRON.getAttackDamageBonus(), 5),
+	//
+	SILVER(2, (int) (ItemTier.IRON.getUses() * 1.3), ItemTier.GOLD.getSpeed(), ItemTier.GOLD.getAttackDamageBonus(), 5),
+	//
+	TITANIUM(2, ItemTier.IRON.getUses() * 4, ItemTier.IRON.getSpeed() * 1.1f, ItemTier.IRON.getAttackDamageBonus() * 1.6f, 5);
+	//
 
-    private final int harvestLevel;
-    private final int maxUses;
-    private final float efficency;
-    private final float attackDammage;
-    private final int enchantability;
+	private final int harvestLevel;
+	private final int maxUses;
+	private final float efficency;
+	private final float attackDammage;
+	private final int enchantability;
 
-    ExtendedItemTier(int harvestLevel, int maxUses, float efficency, float attackDammage, int enchantability) {
-	this.harvestLevel = harvestLevel;
-	this.maxUses = maxUses;
-	this.efficency = efficency;
-	this.attackDammage = attackDammage;
-	this.enchantability = enchantability;
-    }
+	ExtendedItemTier(int harvestLevel, int maxUses, float efficency, float attackDammage, int enchantability) {
+		this.harvestLevel = harvestLevel;
+		this.maxUses = maxUses;
+		this.efficency = efficency;
+		this.attackDammage = attackDammage;
+		this.enchantability = enchantability;
+	}
 
-    @Override
-    public int getMaxUses() {
-	return maxUses;
-    }
+	@Override
+	public int getUses() {
+		return maxUses;
+	}
 
-    @Override
-    public float getEfficiency() {
-	return efficency;
-    }
+	@Override
+	public float getSpeed() {
+		return efficency;
+	}
 
-    @Override
-    public float getAttackDamage() {
-	return attackDammage;
-    }
+	@Override
+	public float getAttackDamageBonus() {
+		return attackDammage;
+	}
 
-    @Override
-    public int getHarvestLevel() {
-	return harvestLevel;
-    }
+	@Override
+	public int getLevel() {
+		return harvestLevel;
+	}
 
-    @Override
-    public int getEnchantability() {
-	return enchantability;
-    }
+	@Override
+	public int getEnchantmentValue() {
+		return enchantability;
+	}
 
-    @Override
-    public Ingredient getRepairMaterial() {
-	return Ingredient.EMPTY;
-    }
+	@Override
+	public Ingredient getRepairIngredient() {
+		return Ingredient.EMPTY;
+	}
 
-    public String tag() {
-	return name().toLowerCase();
-    }
+	public String tag() {
+		return name().toLowerCase();
+	}
 
 }
--- src/main/java/extendedtools/common/item/PaxelItem.java
+++ src/main/java/extendedtools/common/item/PaxelItem.java
@@ -11,6 +11,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.CampfireBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
@@ -27,76 +28,70 @@ import net.minecraftforge.common.util.Constants.BlockFlags;
 import net.minecraftforge.common.util.Constants.WorldEvents;
 
 public class PaxelItem extends ToolItem {
-    private static final Set<Material> AXE_EFFECTIVE_ON_MATERIALS = Sets.newHashSet(Material.WOOD, Material.NETHER_WOOD, Material.PLANTS,
-	    Material.TALL_PLANTS, Material.BAMBOO, Material.GOURD);
+	private static final Set<Material> AXE_EFFECTIVE_ON_MATERIALS = Sets.newHashSet(Material.WOOD, Material.NETHER_WOOD, Material.PLANT, Material.REPLACEABLE_PLANT, Material.BAMBOO, Material.VEGETABLE);
 
-    public PaxelItem(IItemTier tier, Properties prop) {
-	super(4, -2.4f, tier, Collections.emptySet(),
-		prop.addToolType(ToolType.AXE, tier.getHarvestLevel()).addToolType(ToolType.HOE, tier.getHarvestLevel())
-			.addToolType(ToolType.PICKAXE, tier.getHarvestLevel()).addToolType(ToolType.SHOVEL, tier.getHarvestLevel())
-			.maxDamage(tier.getMaxUses() * 2));
-    }
-
-    @Override
-    public boolean canHarvestBlock(BlockState state) {
-	ToolType harvestTool = state.getHarvestTool();
-	if ((harvestTool == ToolType.AXE || harvestTool == ToolType.PICKAXE || harvestTool == ToolType.SHOVEL)
-		&& getTier().getHarvestLevel() >= state.getHarvestLevel()) {
-	    return true;
+	public PaxelItem(IItemTier tier, Properties prop) {
+		super(4, -2.4f, tier, Collections.emptySet(), prop.addToolType(ToolType.AXE, tier.getLevel()).addToolType(ToolType.HOE, tier.getLevel()).addToolType(ToolType.PICKAXE, tier.getLevel()).addToolType(ToolType.SHOVEL, tier.getLevel()).durability(tier.getUses() * 2));
 	}
-	if (state.isIn(Blocks.SNOW) || state.isIn(Blocks.SNOW_BLOCK)) {
-	    return true;
-	}
-	Material material = state.getMaterial();
-	return material == Material.ROCK || material == Material.IRON || material == Material.ANVIL;
-    }
 
-    @Override
-    public float getDestroySpeed(@Nonnull ItemStack stack, BlockState state) {
-	Material material = state.getMaterial();
-	if (material == Material.IRON || material == Material.ANVIL || material == Material.ROCK || AXE_EFFECTIVE_ON_MATERIALS.contains(material)
-		|| getToolTypes(stack).stream().anyMatch(state::isToolEffective)) {
-	    return getTier().getEfficiency();
-	}
-	return 1;
-    }
-
-    @Nonnull
-    @Override
-    public ActionResultType onItemUse(ItemUseContext context) {
-	World world = context.getWorld();
-	BlockPos pos = context.getPos();
-	PlayerEntity player = context.getPlayer();
-	ItemStack stack = context.getItem();
-	BlockState state = world.getBlockState(pos);
-	BlockState result = state.getToolModifiedState(world, pos, player, stack, ToolType.AXE);
-	if (result != null) {
-	    world.playSound(player, pos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
-	} else {
-	    if (context.getFace() == Direction.DOWN) {
-		return ActionResultType.PASS;
-	    }
-	    BlockState foundResult = state.getToolModifiedState(world, pos, player, stack, ToolType.SHOVEL);
-	    if (foundResult != null && world.isAirBlock(pos.up())) {
-		world.playSound(player, pos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
-		result = foundResult;
-	    } else if (state.getBlock() instanceof CampfireBlock && state.get(CampfireBlock.LIT) == Boolean.TRUE) {
-		if (!world.isRemote) {
-		    world.playEvent(null, WorldEvents.FIRE_EXTINGUISH_SOUND, pos, 0);
+	@Override
+	public boolean mineBlock(ItemStack stack, World level, BlockState state, BlockPos pos, LivingEntity entity) {
+		ToolType harvestTool = state.getHarvestTool();
+		if ((harvestTool == ToolType.AXE || harvestTool == ToolType.PICKAXE || harvestTool == ToolType.SHOVEL) && getTier().getLevel() >= state.getHarvestLevel()) {
+			return true;
 		}
-		CampfireBlock.extinguish(world, pos, state);
-		result = state.with(CampfireBlock.LIT, false);
-	    }
+		if (state.is(Blocks.SNOW) || state.is(Blocks.SNOW_BLOCK)) {
+			return true;
+		}
+		Material material = state.getMaterial();
+		return material == Material.STONE || material == Material.METAL || material == Material.HEAVY_METAL;
 	}
-	if (result == null) {
-	    return ActionResultType.PASS;
+
+	@Override
+	public float getDestroySpeed(@Nonnull ItemStack stack, BlockState state) {
+		Material material = state.getMaterial();
+		if (material == Material.METAL || material == Material.HEAVY_METAL || material == Material.STONE || AXE_EFFECTIVE_ON_MATERIALS.contains(material) || getToolTypes(stack).stream().anyMatch(state::isToolEffective)) {
+			return getTier().getSpeed();
+		}
+		return 1;
 	}
-	if (!world.isRemote) {
-	    world.setBlockState(pos, result, BlockFlags.DEFAULT_AND_RERENDER);
-	    if (player != null) {
-		stack.damageItem(1, player, onBroken -> onBroken.sendBreakAnimation(context.getHand()));
-	    }
+	
+	@Override
+	public ActionResultType useOn(ItemUseContext context) {
+		World world = context.getLevel();
+		BlockPos pos = context.getClickedPos();
+		PlayerEntity player = context.getPlayer();
+		ItemStack stack = context.getItemInHand();
+		BlockState state = world.getBlockState(pos);
+		BlockState result = state.getToolModifiedState(world, pos, player, stack, ToolType.AXE);
+		if (result != null) {
+			world.playSound(player, pos, SoundEvents.AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+		} else {
+			if (context.getClickedFace() == Direction.DOWN) {
+				return ActionResultType.PASS;
+			}
+			BlockState foundResult = state.getToolModifiedState(world, pos, player, stack, ToolType.SHOVEL);
+			if (foundResult != null && state.isAir(world, pos)) {
+				world.playSound(player, pos, SoundEvents.SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
+				result = foundResult;
+			} else if (state.getBlock() instanceof CampfireBlock && state.getValue(CampfireBlock.LIT) == Boolean.TRUE) {
+				if (!world.isClientSide) {
+					world.levelEvent(null, WorldEvents.FIRE_EXTINGUISH_SOUND, pos, 0);
+				}
+				CampfireBlock.dowse(world, pos, state);
+				result = state.setValue(CampfireBlock.LIT, false);
+			}
+		}
+		if (result == null) {
+			return ActionResultType.PASS;
+		}
+		if (!world.isClientSide) {
+			world.setBlock(pos, result, BlockFlags.DEFAULT_AND_RERENDER);
+			if (player != null) {
+				stack.hurtAndBreak(1, player, onBroken -> onBroken.broadcastBreakEvent(context.getHand()));
+			}
+		}
+		return ActionResultType.sidedSuccess(world.isClientSide);
 	}
-	return ActionResultType.func_233537_a_(world.isRemote);
-    }
+
 }
